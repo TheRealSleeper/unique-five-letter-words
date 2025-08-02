195c1d3312145b46129a74fbaada3003c96a6302
TheRealSleeper <asleeper135@gmail.com>
2025-08-02 12:00:21 -0500

    Working brute force

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use arraystring::ArrayString;
+use arraystring::typenum::U5;
 use itertools::Itertools;
-use rayon::iter::ParallelIterator;
-use rayon::str::ParallelString;
 use std::collections::HashSet;
 use std::ops::Add;
 use std::sync::atomic::Ordering;
@@ -9,24 +9,23 @@ use std::time::Instant;
 
 fn main() {
     let t_start = Instant::now();
-    let words: Arc<Vec<String>> = Arc::from(
-        std::fs::read_to_string("wordle_words.txt")
-            .unwrap()
-            .par_lines()
-            .map(|w| w.to_ascii_lowercase())
-            .filter(|w| w.len() == 5)
-            .filter(|w| {
-                w.chars().sorted_unstable().dedup().count() == 5
-                    && w.chars().all(|c| c.is_ascii_alphabetic())
-            })
-            .collect::<Vec<String>>(),
-    );
+    let words = std::fs::read_to_string("words.txt")
+        .unwrap()
+        .lines()
+        .map(|w| w.to_ascii_lowercase())
+        .filter(|w| w.len() == 5)
+        .filter(|w| {
+            w.chars().sorted_unstable().dedup().count() == 5
+                && w.chars().all(|c| c.is_ascii_alphabetic())
+        })
+        .map(|w| ArrayString::from(w.as_str()))
+        .collect::<Vec<ArrayString<U5>>>();
     println!("{} valid words in list", words.len());
 
     // for word in words.iter() {
     //     println!("{word}");
     // }
-    let combos: Arc<RwLock<HashSet<Vec<String>>>> =
+    let combos: Arc<RwLock<HashSet<[Option<usize>; 5]>>> =
         Arc::new(RwLock::new(HashSet::with_capacity(words.len() / 5)));
     let threads = std::thread::available_parallelism().unwrap().get();
     // println!("{threads} threads available");
@@ -46,13 +45,16 @@ fn main() {
                 if i >= words.len() {
                     break;
                 }
-                println!("At word {i}, {:.3}% complete, {:.1} seconds remaining", 
-                    i as f32 / words.len() as f32 * 100.0, 
-                    1.0 / (i as f32 / words.len() as f32) * t_start.elapsed().as_secs_f32());
+                println!(
+                    "At word {i}, {:.3}% complete, {:.1} seconds remaining",
+                    i.saturating_sub(threads) as f32 / words.len() as f32 * 100.0,
+                    t_start.elapsed().as_secs_f32()
+                        / (i.saturating_sub(threads - 1) as f32 / words.len() as f32)
+                        * (1.0 - i as f32 / words.len() as f32)
+                );
 
-                let found_chars = [false; 26];
-                let found_words = Vec::with_capacity(5);
-                get_words(found_chars, found_words, combos.clone(), words.clone(), 0, i);
+                let found_words = [None; 5];
+                get_words(0, found_words, combos.clone(), &words, 0, i);
             }
         }));
     }
@@ -62,7 +64,14 @@ fn main() {
     }
 
     for set in combos.read().unwrap().iter() {
-        println!("{}, {}, {}, {}, {}", set[0], set[1], set[2], set[3], set[4]);
+        println!(
+            "{}, {}, {}, {}, {}",
+            words[set[0].unwrap()],
+            words[set[1].unwrap()],
+            words[set[2].unwrap()],
+            words[set[3].unwrap()],
+            words[set[4].unwrap()]
+        );
     }
 
     // println!("{}", output.read().unwrap());
@@ -74,67 +83,81 @@ fn main() {
     );
 }
 
-// /// Checks if the characters in a given word have been used in another word.
-// /// If not, it adds the characters of the word to the found character array
-// fn check_chars(found_chars: &mut [bool; 26], word: &str) -> bool {
-//     // print!("Checking {}: ", word);
-//     if word
-//         .chars()
-//         .all(|c| !found_chars[(c as u8 - b'a') as usize])
-//     {
-//         for c in word.bytes() {
-//             found_chars[(c - b'a') as usize] = true;
-//         }
-//         // print!("{word} passed!\n");
-//         // println!(
-//         //     "Letters '{}' still available",
-//         //     found_chars
-//         //         .iter()
-//         //         .enumerate()
-//         //         .filter(|(_, c)| !**c)
-//         //         .map(|(i, _)| (i as u8 + 'a' as u8) as char)
-//         //         .collect::<String>()
-//         // );
-//         true
-//     } else {
-//         // print!("Failed!\n");
-//         false
-//     }
-// }
+trait IndexBits {
+    fn set_bit(&mut self, n: u8, val: bool) -> Self;
+    fn get_bit(self, n: u8) -> bool;
+}
+
+impl IndexBits for u32 {
+    #[inline]
+    fn set_bit(&mut self, n: u8, val: bool) -> Self {
+        if val {
+            *self |= 1 << n;
+        } else {
+            *self &= !(1 << n);
+        }
+
+        *self
+    }
+
+    #[inline]
+    fn get_bit(self, n: u8) -> bool {
+        (self >> n & 1) == 1
+    }
+}
 
 fn get_words(
-    found_chars: [bool; 26],
-    found_words: Vec<String>,
-    combos: Arc<RwLock<HashSet<Vec<String>>>>,
-    words: Arc<Vec<String>>, 
-    iteration: usize,
-    word_position: usize
+    found_chars: u32,
+    found_words: [Option<usize>; 5],
+    combos: Arc<RwLock<HashSet<[Option<usize>; 5]>>>,
+    words: &Vec<ArrayString<U5>>,
+    depth: usize,
+    word_position: usize,
 ) {
     if words[word_position]
         .chars()
-        .all(|c| !found_chars[(c as u8 - b'a') as usize])
+        .all(|c| !found_chars.get_bit(c as u8 - b'a'))
     {
-        let mut found_words = found_words.clone(); 
-        found_words.push(words[word_position].clone());
-        
+        let mut found_words = found_words;
+        found_words[depth] = Some(word_position);
+
         let mut found_chars = found_chars;
         for c in words[word_position].bytes() {
-            found_chars[(c - b'a') as usize] = true;
+            found_chars.set_bit(c - b'a', true);
         }
-        
-        if iteration < 5 {
+
+        if depth < 4 {
             for ii in word_position.add(1)..words.len() {
-                get_words(found_chars, found_words.clone(), combos.clone(), words.clone(), iteration + 1, ii);
+                get_words(
+                    found_chars,
+                    found_words,
+                    combos.clone(),
+                    words,
+                    depth + 1,
+                    ii,
+                );
             }
         } else {
-            found_words.sort(); 
-            println!("{}, {}, {}, {}, {}", 
-                found_words[0], 
-                found_words[1], 
-                found_words[2], 
-                found_words[3], 
-                found_words[4]);
+            found_words.sort();
+            println!(
+                "{}, {}, {}, {}, {}",
+                words[found_words[0].unwrap()],
+                words[found_words[1].unwrap()],
+                words[found_words[2].unwrap()],
+                words[found_words[3].unwrap()],
+                words[found_words[4].unwrap()]
+            );
             combos.write().unwrap().insert(found_words);
         }
-    } 
+    }
+}
+
+#[test]
+fn index_int_test() {
+    assert_eq!(3, 1.set_bit(1, true));
+    assert_eq!(8, 0.set_bit(3, true));
+    assert_eq!(0, 1.set_bit(0, false));
+    assert_eq!(1, 3.set_bit(1, false));
+    assert_eq!(true, 1.get_bit(0));
+    assert_eq!(false, 1.get_bit(1));
 }
