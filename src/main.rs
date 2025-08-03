97cd36a59edcada99aa26cf910849f063f0f8a50
TheRealSleeper <asleeper135@gmail.com>
2025-08-02 23:06:51 -0500

    Drastically faster than original (~56x), still slow

--- src/main.rs
+++ src/main.rs
@@ -1,163 +1,134 @@
-use arraystring::ArrayString;
-use arraystring::typenum::U5;
 use itertools::Itertools;
-use std::collections::HashSet;
-use std::ops::Add;
-use std::sync::atomic::Ordering;
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
+use std::sync::atomic::AtomicUsize;
 use std::time::Instant;
 
 fn main() {
     let t_start = Instant::now();
-    let words = std::fs::read_to_string("words.txt")
-        .unwrap()
-        .lines()
-        .map(|w| w.to_ascii_lowercase())
-        .filter(|w| w.len() == 5)
-        .filter(|w| {
-            w.chars().sorted_unstable().dedup().count() == 5
-                && w.chars().all(|c| c.is_ascii_alphabetic())
-        })
-        .map(|w| ArrayString::from(w.as_str()))
-        .collect::<Vec<ArrayString<U5>>>();
-    println!("{} valid words in list", words.len());
+    let words = Arc::new(
+        std::fs::read_to_string("words_alpha.txt")
+            .unwrap()
+            .lines()
+            .map(|w| w.trim())
+            .filter(|w| w.len() == 5)
+            .sorted()
+            .map(|w| {
+                (
+                    w.to_string(),
+                    w.chars().fold(0_u32, |acc, c| acc | 1 << c as u8 - b'a'),
+                )
+            })
+            .sorted_unstable_by(|a, b| a.0.cmp(&b.0))
+            .collect_vec(),
+    );
 
-    // for word in words.iter() {
-    //     println!("{word}");
-    // }
-    let combos: Arc<RwLock<HashSet<[Option<usize>; 5]>>> =
-        Arc::new(RwLock::new(HashSet::with_capacity(words.len() / 5)));
-    let threads = std::thread::available_parallelism().unwrap().get();
-    // println!("{threads} threads available");
-    let i_shared = Arc::new(std::sync::atomic::AtomicUsize::new(0));
-    // let output: Arc<RwLock<String>> = Arc::new(RwLock::new(String::new()));
-
-    let mut handles = Vec::with_capacity(threads);
-    for _ in 0..threads {
-        let combos = combos.clone();
-        // let output = output.clone();
-        let words = words.clone();
-        let i_shared = i_shared.clone();
-
-        handles.push(std::thread::spawn(move || {
-            while i_shared.load(Ordering::Acquire) < words.len() {
-                let i = i_shared.fetch_add(1, Ordering::AcqRel);
-                if i >= words.len() {
-                    break;
-                }
-                println!(
-                    "At word {i}, {:.3}% complete, {:.1} seconds remaining",
-                    i.saturating_sub(threads) as f32 / words.len() as f32 * 100.0,
-                    t_start.elapsed().as_secs_f32()
-                        / (i.saturating_sub(threads - 1) as f32 / words.len() as f32)
-                        * (1.0 - i as f32 / words.len() as f32)
-                );
-
-                let found_words = [None; 5];
-                get_words(0, found_words, combos.clone(), &words, 0, i);
-            }
-        }));
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    for set in combos.read().unwrap().iter() {
-        println!(
-            "{}, {}, {}, {}, {}",
-            words[set[0].unwrap()],
-            words[set[1].unwrap()],
-            words[set[2].unwrap()],
-            words[set[3].unwrap()],
-            words[set[4].unwrap()]
+    let char_masks = words
+        .iter()
+        .map(|w| w.1)
+        .sorted_unstable()
+        .dedup()
+        .collect_vec();
+    let mut filtered_words = std::collections::HashMap::new();
+    for mask in char_masks.iter() {
+        filtered_words.insert(
+            *mask,
+            words
+                .iter()
+                .filter_map(|(w, m)| (*mask == *m).then_some(w))
+                .cloned()
+                .collect_vec(),
         );
     }
+    let filtered_words = Arc::new(filtered_words);
 
-    // println!("{}", output.read().unwrap());
-
+    let threads = std::thread::available_parallelism().unwrap().get();
     println!(
-        "Found {} sets of five letter words made up of 25 unique letters in {:.3} seconds",
-        combos.read().unwrap().len(),
-        t_start.elapsed().as_secs_f64()
+        "{} valid words in list, and {threads} threads available",
+        words.len()
     );
-}
 
-trait IndexBits {
-    fn set_bit(&mut self, n: u8, val: bool) -> Self;
-    fn get_bit(self, n: u8) -> bool;
-}
+    let i_shared = Arc::new(AtomicUsize::new(0));
+    let mut handles = Vec::new();
+    for _ in 0..threads {
+        let filtered_words = filtered_words.clone();
+        let i_shared = i_shared.clone();
+        let masks = char_masks.clone();
 
-impl IndexBits for u32 {
-    #[inline]
-    fn set_bit(&mut self, n: u8, val: bool) -> Self {
-        if val {
-            *self |= 1 << n;
-        } else {
-            *self &= !(1 << n);
-        }
-
-        *self
-    }
-
-    #[inline]
-    fn get_bit(self, n: u8) -> bool {
-        (self >> n & 1) == 1
-    }
-}
-
-fn get_words(
-    found_chars: u32,
-    found_words: [Option<usize>; 5],
-    combos: Arc<RwLock<HashSet<[Option<usize>; 5]>>>,
-    words: &Vec<ArrayString<U5>>,
-    depth: usize,
-    word_position: usize,
-) {
-    if words[word_position]
-        .chars()
-        .all(|c| !found_chars.get_bit(c as u8 - b'a'))
-    {
-        let mut found_words = found_words;
-        found_words[depth] = Some(word_position);
-
-        let mut found_chars = found_chars;
-        for c in words[word_position].bytes() {
-            found_chars.set_bit(c - b'a', true);
-        }
-
-        if depth < 4 {
-            for ii in word_position.add(1)..words.len() {
-                get_words(
-                    found_chars,
-                    found_words,
-                    combos.clone(),
-                    words,
-                    depth + 1,
-                    ii,
-                );
+        handles.push(std::thread::spawn(move || {
+            let mut sets = Vec::new();
+            while i_shared.load(std::sync::atomic::Ordering::Acquire) < filtered_words.len() {
+                let mut mask_sets = Vec::new();
+                let i = i_shared.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+                println!("Checking from {i} of {} masks", filtered_words.len());
+                for ii in (i + 1)..masks.len() {
+                    if (masks[i] | masks[ii]).count_ones() != 10 {
+                        continue;
+                    }
+                    for iii in (ii + 1)..masks.len() {
+                        if (masks[i] | masks[ii] | masks[iii]).count_ones() != 15 {
+                            continue;
+                        }
+                        for iii in (ii + 1)..masks.len() {
+                            if (masks[i] | masks[ii] | masks[iii]).count_ones() != 15 {
+                                continue;
+                            }
+                            for iv in (iii + 1)..masks.len() {
+                                if (masks[i] | masks[ii] | masks[iii] | masks[iv]).count_ones()
+                                    != 20
+                                {
+                                    continue;
+                                }
+                                for v in (iv + 1)..masks.len() {
+                                    if (masks[i] | masks[ii] | masks[iii] | masks[iv] | masks[v])
+                                        .count_ones()
+                                        != 25
+                                    {
+                                        continue;
+                                    }
+                                    mask_sets.push([
+                                        masks[i], masks[ii], masks[iii], masks[iv], masks[v],
+                                    ]);
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                // for mask in masks[i..masks.len()]
+                //     .iter()
+                //     .combinations(5)
+                //     .filter(|combo| combo.iter().fold(0, |acc, n| acc | **n) == 25)
+                // {
+                // 
+                for mask in mask_sets.into_iter().sorted_unstable().dedup() {
+                    for word1 in filtered_words[&mask[0]].iter() {
+                        for word2 in filtered_words[&mask[1]].iter() {
+                            for word3 in filtered_words[&mask[2]].iter() {
+                                for word4 in filtered_words[&mask[3]].iter() {
+                                    for word5 in filtered_words[&mask[4]].iter() {
+                                        sets.push(format!(
+                                            "{word1}, {word2}, {word3}, {word4}, {word5}"
+                                        ));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
-        } else {
-            found_words.sort();
-            println!(
-                "{}, {}, {}, {}, {}",
-                words[found_words[0].unwrap()],
-                words[found_words[1].unwrap()],
-                words[found_words[2].unwrap()],
-                words[found_words[3].unwrap()],
-                words[found_words[4].unwrap()]
-            );
-            combos.write().unwrap().insert(found_words);
-        }
+            sets
+        }));
     }
-}
+    let mut sets = Vec::with_capacity(threads);
+    for handle in handles {
+        sets.push(handle.join().unwrap());
+    }
 
-#[test]
-fn index_int_test() {
-    assert_eq!(3, 1.set_bit(1, true));
-    assert_eq!(8, 0.set_bit(3, true));
-    assert_eq!(0, 1.set_bit(0, false));
-    assert_eq!(1, 3.set_bit(1, false));
-    assert_eq!(true, 1.get_bit(0));
-    assert_eq!(false, 1.get_bit(1));
+    let answer = sets.iter().flatten().sorted().format("\n").to_string();
+    std::fs::write("answer.txt", &answer).unwrap();
+    println!(
+        "found {} solutions in {:.3} seconds",
+        answer.lines().count(),
+        t_start.elapsed().as_secs_f32()
+    );
 }
