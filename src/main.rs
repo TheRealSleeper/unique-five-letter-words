c03dfe0be0ab1c59190891e935f95352cfa9708a
TheRealSleeper <asleeper135@gmail.com>
2025-08-23 09:49:37 -0500

    Reasonably fast (~3.5 seconds on Ryzen 9950X3D)

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-use anyhow::Result;
 use arraystring::{ArrayString, typenum::U5};
 use itertools::Itertools;
 use rayon::prelude::*;
 use std::time::Instant;
 
-fn main() -> Result<()> {
+fn main() {
     let t_start = Instant::now();
+
+    // Find all valid words and u32 mask where each bit represents a particular letter
     let words: Vec<(ArrayString<U5>, u32)> = std::fs::read_to_string("words_alpha.txt")
         .unwrap()
         .lines()
         .map(|w| w.trim())
-        .filter(|w| w.chars().sorted().dedup().count() == 5)
+        .filter(|w| w.len() == 5)
         .map(|w| {
             (
                 arraystring::ArrayString::from(w),
-                w.chars().fold(0_u32, |acc, c| acc | 1 << c as u8 - b'a'),
+                w.chars().fold(0_u32, |acc, c| acc | (1 << c as u8 - b'a')),
             )
         })
+        .filter(|w| w.1.count_ones() == 5)
         .sorted_unstable_by(|a, b| a.0.cmp(&b.0))
         .collect_vec();
 
+    // Collect all unique masks found in word set
     let char_masks = words
         .iter()
         .map(|w| w.1)
@@ -27,6 +30,7 @@ fn main() -> Result<()> {
         .dedup()
         .collect_vec();
 
+    // Collect words into groups sharing the same masks
     let mut filtered_words = std::collections::HashMap::new();
     for &mask in char_masks.iter() {
         filtered_words.insert(
@@ -39,73 +43,31 @@ fn main() -> Result<()> {
         );
     }
 
-    // let answers =
-    //     char_masks
-    //         .par_iter()
-    //         .enumerate()
-    //         .map(|(i, &m)| {
-    //             let mut masks2 = Vec::with_capacity(char_masks.len());
-    //             let mut masks3 = Vec::with_capacity(char_masks.len());
-    //             let mut masks4 = Vec::with_capacity(char_masks.len());
+    #[cfg(debug_assertions)]
+    let count = std::sync::Arc::new(std::sync::atomic::AtomicUsize::new(0));
 
-    //             let filter =
-    //                 |combined_mask: u32,
-    //                  current_mask: u32,
-    //                  previous_mask: u32,
-    //                  masks: &[u32],
-    //                  filtered_masks: &mut Vec<u32>| {
-    //                     filtered_masks.clear();
-    //                     filtered_masks.extend(masks.iter().copied().filter(|&mask| {
-    //                         combined_mask & mask == 0 && current_mask > previous_mask
-    //                     }));
-    //                 };
-
-    //             let mut solutions = Vec::new();
-
-    //             println!("{i}");
-
-    //             for &mask1 in char_masks[i + 1..].iter() {
-    //                 filter(mask1, mask1, 0, &char_masks, &mut masks2);
-    //                 for &mask2 in masks2.iter() {
-    //                     let combined_mask = mask1 | mask2;
-    //                     filter(combined_mask, mask2, mask1, &char_masks, &mut masks3);
-    //                     for &mask3 in masks3.iter() {
-    //                         let combined_mask = combined_mask | mask3;
-    //                         filter(combined_mask, mask3, mask2, &char_masks, &mut masks4);
-    //                         for &mask4 in masks4.iter() {
-    //                             if mask4 & combined_mask == 0 && mask4 > mask4 {
-    //                                 // println!("Found masks");
-    //                                 solutions.push([m, mask1, mask2, mask3, mask4]);
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-
-    //             solutions
-    //         })
-    //         .flatten()
-    //         .collect::<Vec<[u32; 5]>>();
-
-    // let combo_count = char_masks.iter().combinations(5).count();
-
+    // Find all mask combinations that solve the challenge
     let combos = char_masks
         .par_iter()
         .enumerate()
         .map(|(i, &mask1)| {
-            println!("At {i} of {}", char_masks.len());
-            let mut combos = Vec::with_capacity(20);
+            // println!("At {i} of {}", char_masks.len());
+            let mut combos = Vec::with_capacity(10);
             let combo_mask = mask1;
-            for (ii, &mask2) in char_masks[(i + 1)..].iter().enumerate() {
+            for ii in (i + 1)..char_masks.len() {
+                let mask2 = char_masks[ii];
                 if combo_mask & mask2 == 0 {
                     let combo_mask = combo_mask | mask2;
-                    for (iii, &mask3) in char_masks[(ii + 1)..].iter().enumerate() {
+                    for iii in (ii + 1)..char_masks.len() {
+                        let mask3 = char_masks[iii];
                         if combo_mask & mask3 == 0 {
                             let combo_mask = combo_mask | mask3;
-                            for (iv, &mask4) in char_masks[(iii + 1)..].iter().enumerate() {
+                            for iv in (iii + 1)..char_masks.len() {
+                                let mask4 = char_masks[iv];
                                 if combo_mask & mask4 == 0 {
                                     let combo_mask = combo_mask | mask4;
-                                    for &mask5 in char_masks[(iv + 1)..].iter() {
+                                    for v in (iv + 1)..char_masks.len() {
+                                        let mask5 = char_masks[v];
                                         if combo_mask & mask5 == 0 {
                                             combos.push([mask1, mask2, mask3, mask4, mask5]);
                                         }
@@ -116,56 +78,24 @@ fn main() -> Result<()> {
                     }
                 }
             }
+            #[cfg(debug_assertions)]
+            {
+                let num_completed = count.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+                let percent_complete = num_completed as f32 / char_masks.len() as f32 * 100.0;
+                let elapsed = t_start.elapsed();
+                let time_remaining = elapsed.as_secs_f32() / num_completed as f32 * (char_masks.len() - num_completed) as f32;
+                println!(
+                    "Finished {num_completed} of {} masks, {percent_complete:.2}% complete, estimated {time_remaining:.0} seconds remaining",
+                    char_masks.len()
+                );
+            }
+
             combos
         })
         .flatten()
         .collect::<Vec<_>>();
 
-    // let combos: Vec<[u32; 5]> = char_masks
-    //         .par_iter()
-    //         .flat_map(|&m1| {
-    //             let mask1 = m1;
-    //             let mut uniq_masks2 = Vec::with_capacity(char_masks.len());
-    //             let mut uniq_masks3 = Vec::with_capacity(char_masks.len());
-    //             let mut uniq_masks4 = Vec::with_capacity(char_masks.len());
-
-    //             let filter = |mask: u32, m: u32, uniq: &[u32], filtered: &mut Vec<u32>| {
-    //                 filtered.clear();
-    //                 filtered.extend(
-    //                     uniq
-    //                         .iter()
-    //                         .copied()
-    //                         .take_while(move |m2| *m2 < m) // Strictly descending to avoid permutations.
-    //                         .filter(move |m2| m2 & mask == 0) // Empty intersection to avoid duplicate letters.
-    //                 );
-    //             };
-
-    //             let mut solutions = Vec::new();
-    //             filter(mask1, m1, &char_masks, &mut uniq_masks2);
-    //             for &m2 in &uniq_masks2 {
-    //                 let mask2 = mask1 | m2;
-    //                 filter(mask2, m2, &uniq_masks2, &mut uniq_masks3);
-    //                 for &m3 in &uniq_masks3 {
-    //                     let mask3 = mask2 | m3;
-    //                     filter(mask3, m3, &uniq_masks3, &mut uniq_masks4);
-    //                     for &m4 in &uniq_masks4 {
-    //                         let mask4 = mask3 | m4;
-    //                         for &m5 in &uniq_masks4 {
-    //                             if m5 > m4 {
-    //                                 break;
-    //                             }
-
-    //                             if m5 & mask4 == 0 {
-    //                                 solutions.push([m1, m2, m3, m4, m5]);
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //             solutions
-    //         })
-    //         .collect();
-
+    // Make every possible word combination from valid mask combinations
     let answer = combos
         .iter()
         .map(|masks| {
@@ -173,10 +103,11 @@ fn main() -> Result<()> {
                 .iter()
                 .map(|mask| filtered_words.get(mask).unwrap())
                 .multi_cartesian_product()
-                .map(|v| v.into_iter().format(", "))
+                .map(|v| v.into_iter().format(", ").to_string())
                 .collect_vec()
         })
         .flatten()
+        .sorted()
         .format("\n")
         .to_string();
 
@@ -186,6 +117,4 @@ fn main() -> Result<()> {
         answer.lines().count(),
         t_start.elapsed().as_secs_f32()
     );
-
-    Ok(())
 }
